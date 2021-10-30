48282352cd6efc31a55be58246f6ec9b68b7fd21
Daniele Zago <zagodaniele.9@gmail.com>
2021-10-30 15:31:55 +0200

    up sync CV

--- update-files.sh
+++ update-files.sh
@@ -7,8 +7,7 @@ for d in ${HOME}/Documents/MEGA/main/2021-22/*/; do
     echo "--- Sending $dname ---"
     rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/$dname/build/main.pdf files/pdf/notes/$dname.pdf
 done
-# rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/functional-analysis/build/main.pdf files/pdf/notes/functional-analysis.pdf
-# rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/probability-theory/build/main.pdf files/pdf/notes/probability-theory.pdf
-# rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/applied-multivariate-techniques/build/main.pdf files/pdf/notes/applied-multivariate-techniques.pdf
-# rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/theory-methods-inference/build/main.pdf files/pdf/notes/theory-methods-inference.pdf
-# rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/time-series/build/main.pdf files/pdf/notes/time-series.pdf
+
+echo "--- Sending CV ---"
+rsync -urtv --delete ${HOME}/Documents/git/CV/build/zago_cv.pdf files/pdf/zago_cv.pdf
+
