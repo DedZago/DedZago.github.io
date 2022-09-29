d0653501b2188475c1c9eade150bd50d5a1820f5
Daniele Zago - ST <zagodaniele.9@gmail.com>
2022-09-29 12:27:14 +0200

    update cv

--- update-files.sh
+++ update-files.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/bash
 
 # Update all courses from 2021-22
-for d in ${HOME}/Documents/MEGA/main/2021-22/*/; do
+for d in ${HOME}/Documents/MEGA/main/courses/2021-22/*/; do
     dname=${d#"$(dirname $d)/"}   # subtract prefix
     dname=${dname%"/"}            # subtract postfix
     echo "--- Sending $dname ---"
-    rsync -urtv --delete ${HOME}/Documents/MEGA/main/2021-22/$dname/build/main.pdf ${HOME}/Documents/git/DedZago.github.io/files/pdf/notes/$dname.pdf
+    rsync -urtv --delete ${HOME}/Documents/MEGA/main/courses/2021-22/$dname/build/main.pdf ${HOME}/Documents/git/DedZago.github.io/files/pdf/notes/$dname.pdf
 done
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-for d in ${HOME}/Documents/MEGA/main/Other-courses/*/; do
+for d in ${HOME}/Documents/MEGA/main/courses/Other-courses/*/; do
     dname=${d#"$(dirname $d)/"}   # subtract prefix
     dname=${dname%"/"}            # subtract postfix
     echo "--- Sending $dname ---"
-    rsync -urtv --delete ${HOME}/Documents/MEGA/main/Other-courses/$dname/build/main.pdf ${HOME}/Documents/git/DedZago.github.io/files/pdf/notes/$dname.pdf
+    rsync -urtv --delete ${HOME}/Documents/MEGA/main/courses/Other-courses/$dname/build/main.pdf ${HOME}/Documents/git/DedZago.github.io/files/pdf/notes/$dname.pdf
 done
 IFS=$SAVEIFS
 
